3f49260f62bd26b77e467040a6ef875ba9e32112
sehbazm <sehbazm@gmail.com>
2024-01-03 00:35:55 +0000

    Code updated for visitor identification

--- conversation_visitor_identity.go
+++ conversation_visitor_identity.go
@@ -15,14 +15,14 @@ type IdentificationTokenRequest struct {
 }
 
 type VisitorIdentificationService interface {
-	GenerateIdentificationToken(request IdentificationTokenRequest) (*IdentificationTokenResponse, error)
+	GenerateIdentificationToken(request *IdentificationTokenRequest) (*IdentificationTokenResponse, error)
 }
 
 type VisitorIdentificationServiceOp struct {
 	client *Client
 }
 
-func (s *VisitorIdentificationServiceOp) GenerateIdentificationToken(request IdentificationTokenRequest) (*IdentificationTokenResponse, error) {
+func (s *VisitorIdentificationServiceOp) GenerateIdentificationToken(request *IdentificationTokenRequest) (*IdentificationTokenResponse, error) {
 	response := &IdentificationTokenResponse{}
 	path := visitorIdentificationBasePath + "/tokens/create"
 	if err := s.client.Post(path, request, response); err != nil {
